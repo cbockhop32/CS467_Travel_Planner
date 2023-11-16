287130094cd27bf9c7dc5a89df52f276c0508a19
cbockhop32 <66850542+cbockhop32@users.noreply.github.com>
2023-11-16 07:51:51 -0800

    right container now grabs Trips data and generates ExperienceDetails page

--- front-end/src/RightContainer/RightContainer.jsx
+++ front-end/src/RightContainer/RightContainer.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-
+import React,{useContext} from 'react'
 import { Tab } from 'react-bootstrap';
+import { TripsContext } from '../Context/TripsContext';
+
 import ExperienceDetails from '../ExperienceDetails/ExperienceDetails';
 
 
 
 function RightContainer() {
+    const {currentTrips} = useContext(TripsContext);
+
     return (  
         <Tab.Content className='m-2 p-2 rounded-3'  style={{ backgroundColor:"#d9d9d9", height:"95%",overflowY:"scroll"}}>
-            <Tab.Pane eventKey="#link1"><ExperienceDetails id = {1} name={"Singapore Gardens"} location={"Singapore"}/></Tab.Pane>
-            <Tab.Pane eventKey="#link2"><ExperienceDetails id = {2} name={"Berlin Wall"} location={"Berlin, Germany"}/></Tab.Pane>
-            <Tab.Pane eventKey="#link3"><ExperienceDetails id = {3} name={"Great Wall of China"} location={"Bejing, China"}/></Tab.Pane>
+            {currentTrips.map((trip,index) => {
+                return (<Tab.Pane eventKey={`#link${index}`}><ExperienceDetails id = {index} name={trip.trip_name} location={"Somewhere"} description={trip.description}/></Tab.Pane>
+                )
+            })}
       </Tab.Content>
     );
 }
