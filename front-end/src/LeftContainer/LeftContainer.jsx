3b72d54199865b42ff951cfa7220793fbba38d45
cbockhop32 <66850542+cbockhop32@users.noreply.github.com>
2023-11-28 18:36:04 -0800

    left container

--- front-end/src/LeftContainer/LeftContainer.jsx
+++ front-end/src/LeftContainer/LeftContainer.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect,useContext} from 'react'
+import React,{useEffect,useContext, useState} from 'react'
 import { ListGroup } from 'react-bootstrap';
 import ExperienceTab from '../ExperienceTab/ExperienceTab';
 import TripTab from '../TripTab/TripTab';
@@ -11,6 +11,7 @@ function LeftContainer({view}) {
 
     const {currentTrips,currentExperiences, updateExperiences} = useContext(ExperiencesContext);
 
+    const [token, setToken_] = useState(localStorage.getItem("token"));
 
 
     useEffect(() => {
@@ -26,36 +27,35 @@ function LeftContainer({view}) {
                 if(e.response.status == 401) {
                     console.log("Login to be able to add Experience")
                 }
-                
-                
                 console.log(e)})
-       
+
+
+            console.log(token)
+                
+        
     },[]);
 
 
 
-        if(view == "experiences") {
-            return (
-                    <ListGroup className='m-2 p-2'  style={{ height:"92%", backgroundColor: "#d9d9d9", overflowY:"scroll"}}>
-                        {currentExperiences.map((experience, index) => {
-                            return (<ExperienceTab key={index} id={index} name={experience.experience_name} location={"Somewhere"} />)
-                            })}
-                    </ListGroup>
-      
-            );
-
-
-        } else if(view == "trips") {
-
-            return(
-            <ListGroup className='m-2 p-2'  style={{ height:"92%", backgroundColor: "#d9d9d9", overflowY:"scroll"}}>
-            {currentTrips.map((trip, index) => {
-                return (<TripTab key={index} id={index} name={trip.trip_name} location={"Somewhere"} />)
-                })}
+    if(view == "experiences") {
+        return (
+                <ListGroup className='m-2 p-2'  style={{ height:"92%", backgroundColor: "#d9d9d9", overflowY:"scroll"}}>
+                    {currentExperiences.map((experience, index) => {
+                        return (<ExperienceTab key={index} id={index} name={experience.experience_name} location={"Somewhere"} />)
+                        })}
                 </ListGroup>
-            );
+        );
 
-        }
+
+    } else if(view == "trips") {
+        return(
+        <ListGroup className='m-2 p-2'  style={{ height:"92%", backgroundColor: "#d9d9d9", overflowY:"scroll"}}>
+        {currentTrips.map((trip, index) => {
+            return (<TripTab key={index} id={index} name={trip.trip_name} location={"Somewhere"} />)
+            })}
+            </ListGroup>
+        );
+    }
        
     
 }
